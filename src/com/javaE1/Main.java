2a4a65148065f1a5d1e70786902b149c7803a0f2
huangchengcheng <dcwong09@gmail.com>
2017-09-12 16:23:03 +0800

    diamond name

--- src/com/javaE1/Main.java
+++ src/com/javaE1/Main.java
@@ -1,17 +1,49 @@
 package com.javaE1;
 
-public class Main {
+import static java.lang.System.*;
 
+public class Main {
     public static void rightTriangle(int n){
         for (int i=0;i<n;i++) {
             for (int j = 0; j <= i; j++) {
-                System.out.print("*");
+                out.print("*");
             }
-            System.out.println("");
+            out.println("");
         }
     }
 
+    private static String myName ="huang";
+    private static void printLine(int level,int n){
+        int dot=level*2-1;
+        int space=n-level;
+        for (int i=0;i<space;i++){
+            out.print(" ");
+        }
+        for (int i=0;i<dot;i++){
+            out.print("*");
+        }
+        for (int i=0;i<space;i++){
+            out.print(" ");
+        }
+        out.println("");
+    }
+    private static void R_diamondName(int level,int n){ //starts from 1
+        if(level<n){
+            printLine(level,n);
+            R_diamondName(level+1,n);
+            printLine(level,n);
+        }
+        else {
+            out.println(myName);
+        }
+    }
+    public static void diamondName(int n){
+        R_diamondName(1,n);
+    }
+
+
     public static void main(String[] args) {
-        rightTriangle(3);
+        //rightTriangle(3);
+        diamondName(3);
     }
 }
