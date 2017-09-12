de318b1681f2ee102ae2a78b456bbb02fb9f4342
huangchengcheng <dcwong09@gmail.com>
2017-09-12 15:58:19 +0800

    right truangle

--- src/com/javaE1/Main.java
+++ src/com/javaE1/Main.java
@@ -2,7 +2,16 @@ package com.javaE1;
 
 public class Main {
 
+    public static void rightTriangle(int n){
+        for (int i=0;i<n;i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        rightTriangle(3);
     }
 }
